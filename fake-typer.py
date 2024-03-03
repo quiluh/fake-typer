a6a2052a2025e286bf7445832f677f76ca59abb3
Clyde Mesa <95511845+AquilaPebble@users.noreply.github.com>
2024-03-03 22:27:44 +1300

    fixed index variable bug but there is still some bugs that i cant see because both real and stimulated keys are pressed at the same time

--- fake-typer.py
+++ fake-typer.py
@@ -1,13 +1,12 @@
 import keyboard
 
-file = open("connections-essay.txt","r").read().split()
-index = 0
+file = [open("connections-essay.txt","r").read(),0]
 
 def keyPress(event):
     if event.name != "backspace":
-        print(file[index])
-        index += 1 if index < len(file) else 0
+        keyboard.press_and_release(file[0][file[1]])
+        file[1] += 1 if file[1] < len(file) else 0
     else:
-        index -= 1 if index > 0 else 0
+        file[1] -= 1 if file[1] > 0 else 0
 keyboard.on_press(keyPress)
 keyboard.wait("esc")
