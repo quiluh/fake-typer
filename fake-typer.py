6c4536af0be7d6fd7c0a345c8249cf0bf305c330
Clyde Mesa <95511845+AquilaPebble@users.noreply.github.com>
2024-03-03 22:19:05 +1300

    created file reader, ignores backspace but theres index variable is inaccessible by keyPress(event)

--- fake-typer.py
+++ fake-typer.py
@@ -1,5 +1,13 @@
 import keyboard
+
+file = open("connections-essay.txt","r").read().split()
+index = 0
+
 def keyPress(event):
-    return
+    if event.name != "backspace":
+        print(file[index])
+        index += 1 if index < len(file) else 0
+    else:
+        index -= 1 if index > 0 else 0
 keyboard.on_press(keyPress)
 keyboard.wait("esc")
