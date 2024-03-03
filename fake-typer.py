b16c3a87225e018caf423e2ac4ec220e1569f2b2
Clyde Mesa <95511845+AquilaPebble@users.noreply.github.com>
2024-03-03 22:32:16 +1300

    fixed small len bug)

--- fake-typer.py
+++ fake-typer.py
@@ -5,8 +5,9 @@ file = [open("connections-essay.txt","r").read(),0]
 def keyPress(event):
     if event.name != "backspace":
         keyboard.press_and_release(file[0][file[1]])
-        file[1] += 1 if file[1] < len(file) else 0
+        file[1] += 1 if file[1] < len(file[0]) else 0
     else:
         file[1] -= 1 if file[1] > 0 else 0
+
 keyboard.on_press(keyPress)
 keyboard.wait("esc")
