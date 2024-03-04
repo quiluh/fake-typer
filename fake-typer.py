91f252083467bbf51d3fdf03f8df54eef5c036c9
Clyde Mesa <95511845+AquilaPebble@users.noreply.github.com>
2024-03-04 22:12:13 +1300

    fixed double output by stimulating backspace

--- fake-typer.py
+++ fake-typer.py
@@ -4,6 +4,7 @@ file = [open("connections-essay.txt","r").read(),0]
 
 def keyPress(event):
     if event.name != "backspace":
+        keyboard.press_and_release("backspace")
         keyboard.press_and_release(file[0][file[1]])
         file[1] += 1 if file[1] < len(file[0]) else 0
     else:
